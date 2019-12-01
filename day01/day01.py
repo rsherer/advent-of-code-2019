5a189803117078feffefc9001e18e37ff2aa2747
Robert Sherer <rsherer@gmail.com>
2019-12-01 00:11:10 -0800

    part 2 answer

--- day01/day01.py
+++ day01/day01.py
@@ -24,17 +24,69 @@ spacecraft?
 '''
 
 import numpy as np
+from typing import List
 
-def fuel_requirment(mass: int) -> int:
+def fuel_requirement(mass: int) -> int:
     return mass // 3 - 2
 
-assert fuel_requirment(12) == 2
-assert fuel_requirment(14) == 2
-assert fuel_requirment(1969) == 654
-assert fuel_requirment(100756) == 33583
+assert fuel_requirement(12) == 2
+assert fuel_requirement(14) == 2
+assert fuel_requirement(1969) == 654
+assert fuel_requirement(100756) == 33583
 
 with open('input.txt') as f:
     module_masses = [int(line.strip()) for line in f]
-    fuel_sum = sum(fuel_requirment(mass) for mass in module_masses)
+    fuel_sum = sum(fuel_requirement(mass) for mass in module_masses)
 
 print(fuel_sum)
+
+'''
+part 2
+
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation 
+Double-Checker stops the launch sequence. Apparently, you forgot to include 
+additional fuel for the fuel you just added.
+
+Fuel itself requires fuel just like a module - take its mass, divide by three, 
+round down, and subtract 2. However, that fuel also requires fuel, and that 
+fuel requires fuel, and so on. Any mass that would require negative fuel should 
+instead be treated as if it requires zero fuel; the remaining mass, if any, is 
+instead handled by wishing really hard, which has no mass and is outside the 
+scope of this calculation.
+
+So, for each module mass, calculate its fuel and add it to the total. Then, 
+treat the fuel amount you just calculated as the input mass and repeat the 
+process, continuing until a fuel requirement is zero or negative. For example:
+
+A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 
+divided by 3 and rounded down is 0, which would call for a negative fuel), so 
+the total fuel required is still just 2.
+At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 
+216 more fuel (654 / 3 - 2). 216 then requires 70 more fuel, which requires 
+21 fuel, which requires 5 fuel, which requires no further fuel. So, the total 
+fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+The fuel required by a module of mass 100756 and its fuel is: 
+33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+'''
+
+def fuel_for_fuel(mass: int) -> int:
+    total = 0
+    
+    fuel_for_fuel = fuel_requirement(mass)
+
+    while fuel_for_fuel > 0:
+        total += fuel_for_fuel
+        fuel_for_fuel = fuel_requirement(fuel_for_fuel)
+
+    return total
+
+assert fuel_for_fuel(14) == 2
+assert fuel_for_fuel(12) == 2
+assert fuel_for_fuel(1969) == 966
+assert fuel_for_fuel(100756) == 50346
+
+with open('input.txt') as f:
+    module_masses = [int(line.strip()) for line in f]
+    total_sum = sum(fuel_for_fuel(mass) for mass in module_masses)
+
+print(total_sum)
