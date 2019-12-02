db588ac0f95ef2b61bf77b07e80438ae4f12b842
Robert Sherer <rsherer@gmail.com>
2019-12-02 11:05:48 -0800

    part 2 completed

--- day02/day02.py
+++ day02/day02.py
@@ -78,3 +78,25 @@ answer would be 1202.)
 '''
 
 P2TEXT = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,10,19,1,19,6,23,2,23,13,27,1,27,5,31,2,31,10,35,1,9,35,39,1,39,9,43,2,9,43,47,1,5,47,51,2,13,51,55,1,55,9,59,2,6,59,63,1,63,5,67,1,10,67,71,1,71,10,75,2,75,13,79,2,79,13,83,1,5,83,87,1,87,6,91,2,91,13,95,1,5,95,99,1,99,2,103,1,103,6,0,99,2,14,0,0]
+output = 19690720
+
+noun = P2TEXT[1]
+verb = P2TEXT[2]
+
+def memory(prog: List[int]) -> Tuple[int, int]:
+    memory_test = [code for code in prog]
+    output = 19690720
+
+    for i in range(100):
+        noun = i
+        for i in range(100):
+            verb = i
+            memory_test[1] = noun
+            memory_test[2] = verb
+            if intcode(memory_test)[0] == output:
+                return noun, verb
+                break
+            memory_test = [code for code in prog]
+        memory_test = [code for code in prog]
+
+print(memory(P2TEXT))
