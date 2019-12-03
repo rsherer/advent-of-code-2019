e7857271190120cf9700d0abc23e9459b43ab838
Robert Sherer <rsherer@gmail.com>
2019-12-03 00:44:05 -0800

    add text for part 2

--- day03/day03.py
+++ day03/day03.py
@@ -55,3 +55,25 @@ with open('input.txt') as f:
 
 print(closest_crossed_wire(PATH1, PATH2))
 
+
+
+'''Day 3, part 2
+
+In the above example, the intersection closest to the central port is reached 
+after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second 
+wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 
+8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 
+steps.
+
+Here are the best steps for the extra examples from above:
+
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+What is the fewest combined steps the wires must take to reach an intersection?
+'''
+
+
