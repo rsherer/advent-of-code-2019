f5484503af855c7fe22cde312664ecd5bffce953
Robert Sherer <rsherer@gmail.com>
2019-12-03 21:14:47 -0800

    initial commit

---
+++ day04/day04.py
@@ -0,0 +1,36 @@
+'''
+--- Day 4: Secure Container ---
+You arrive at the Venus fuel depot only to discover it's protected by a 
+password. The Elves had written the password on a sticky note, but someone 
+threw it out.
+
+However, they do remember a few key facts about the password:
+
+It is a six-digit number.
+The value is within the range given in your puzzle input.
+Two adjacent digits are the same (like 22 in 122345).
+Going from left to right, the digits never decrease; they only ever increase or 
+stay the same (like 111123 or 135679).
+Other than the range rule, the following are true:
+
+111111 meets these criteria (double 11, never decreases).
+223450 does not meet these criteria (decreasing pair of digits 50).
+123789 does not meet these criteria (no double).
+How many different passwords within the range given in your puzzle input meet 
+these criteria?
+
+Your puzzle input is 372304-847060.
+'''
+
+def check_password(pwd: int) -> bool:
+    pass
+
+def repeats(pwd: str) -> bool:
+    for c1, c2 in zip(pwd, pwd[1:]):
+        if c1 == c2:
+            return True
+    return False
+
+assert repeats('111111') == True
+assert repeats('223450') == True
+assert repeats('123789') == False
